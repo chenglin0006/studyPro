fb6e525cffe8c65f591e78adf8a2b763426c426a
chenglin02 <chenglin02@dianping.com>
2017-03-01 17:49:11 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
     historyApiFallback: true,
     noInfo: true
   },
-  devtool: '#eval-source-map'
+  devtool: '#source-map'
 }
 
 if (process.env.NODE_ENV === 'production') {
